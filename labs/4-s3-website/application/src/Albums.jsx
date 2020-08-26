a2c492da798f6aa20736b05a49b73b8837ec2b9e
Rob Kenis <rob.kenis@hotmail.com>
2020-08-26 16:05:06 +0200

    Add button to upload images to an album

--- labs/4-s3-website/application/src/Albums.jsx
+++ labs/4-s3-website/application/src/Albums.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import ImageUploader from "react-images-upload";
 
-export const Album = ({id, name, images}) => (<div key={id} className={"album"}>
+const Album = ({id, name, images}) => (<div key={id} className={"album"}>
     <h2>{name}</h2>
     <div className={"images"}>
         {images.map((img, index) => (
             <img key={index} src={img.url} alt={"No idea what it is about"} width="400"/>))}
     </div>
+    <UploadImage albumId={id}/>
 </div>)
 
 export const Albums = () => {
@@ -21,4 +23,42 @@ export const Albums = () => {
     }, [])
     return (<div className={"albums"}>{albums.map(album => (
         <Album key={album.id} id={album.id} name={album.name} images={album.images}/>))}</div>);
+}
+
+const UploadImage = ({albumId}, props) => {
+    const [pictures, setPictures] = useState([]);
+
+    const onDrop = async picture => {
+        setPictures([...pictures, picture]);
+        const image = await uploadImageToBackend(picture[0]);
+        await addToAlbum(albumId, image.url);
+    };
+    return (
+        <ImageUploader
+            {...props}
+            withIcon={false}
+            withLabel={false}
+            buttonText={"Upload image"}
+            onChange={onDrop}
+            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            maxFileSize={5242880}
+        />
+    );
+}
+
+const uploadImageToBackend = async (picture) => {
+    const formData = new FormData();
+    formData.append('file', picture)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+    const { data } = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/image/_upload`, formData, config);
+    return data;
+}
+
+const addToAlbum = async (albumId, imageUrl) => {
+    console.log(`Adding ${imageUrl} to ${albumId}`);
+    await axios.put(`${process.env.REACT_APP_BACKEND_URL}/albums/${albumId}`, {url: imageUrl});
 }
